2a58b7db95147b5542ed839d880c1078a30743c4
Ramez Chreide <ramiz_chreide@outlook.com>
2025-01-31 16:53:27 +0100

    add: test for fun

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import com.sun.net.httpserver.Authenticator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        System.out.println("You passed the test! Here is a cookie 🍪");
+    }
+}
