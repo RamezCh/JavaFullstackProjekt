d076299f70c8150b33773f72deae8ddc761bc35a
Ramez Chreide <ramiz_chreide@outlook.com>
2025-01-31 16:23:37 +0100

    add: jfiglet

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        String asciiArt = FigletFont.convertOneLine("Hello World!");
+        System.out.println(asciiArt);
+        // mkdir JavaFullstackProjekt
+        // cd JavaFullStackProjekt/src/main/java/org.example
+        // vi Main.java
+        // i
+        // code
+        // javac Main.java
+        // java Main
     }
 }
